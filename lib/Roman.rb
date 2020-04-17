469ff1c1b3347bc1f9a94fc5230fc621692851ed
joannacollier <collj035@gmail.com>
2020-04-17 12:50:57 +0100

    Refactored code

--- lib/Roman.rb
+++ lib/Roman.rb
@@ -1,28 +1,16 @@
 class RomanNumerals 
 
-  def convert(num) 
+  def convert(num)
     if num == 0
       ""
-    elsif num == 1
-      "I"
-    elsif num == 2
-      "II"
-    elsif num == 3
-      "III"
-    elsif num == 4
-      "IIII"
     elsif num == 5
       "V"
-    elsif num == 6
-      "VI"
-    elsif num == 7
-      "VII"
-    elsif num == 8
-      "VIII"
-    elsif num == 9
-      "VIIII"
     elsif num == 10
       "X"
-    end  
-  end
+    elsif num > 4
+      "V" + ("I" * (num - 5))
+    else
+      "I" * num
+    end
+  end 
 end 
